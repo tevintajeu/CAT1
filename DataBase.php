0ce0982570f17a8bcd92540d610dac54cd64a0b6
tevintaje <tevot191030@gmail.com>
2024-10-02 20:58:02 +0300

    another commit

--- DataBase.php
+++ DataBase.php
@@ -3,69 +3,52 @@ require "DataBaseConfig.php";
 
 class DataBase
 {
-    public $connect;
-    public $data;
+    private $conn;
     private $sql;
-    protected $servername;
-    protected $username;
-    protected $password;
-    protected $databasename;
 
-    public function __construct()
+    public function dbConnect()
     {
-        $this->connect = null;
-        $this->data = null;
-        $this->sql = null;
-        $dbc = new DataBaseConfig();
-        $this->servername = $dbc->servername;
-        $this->username = $dbc->username;
-        $this->password = $dbc->password;
-        $this->databasename = $dbc->databasename;
+        $config = new DataBaseConfig();
+        $this->conn = new mysqli($config->servername, $config->username, $config->password, $config->databasename);
+
+        if ($this->conn->connect_error) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
-    function dbConnect()
+    public function prepareData($data)
     {
-        $this->connect = mysqli_connect($this->servername, $this->username, $this->password, $this->databasename);
-        return $this->connect;
+        return mysqli_real_escape_string($this->conn, stripslashes(htmlspecialchars($data)));
     }
 
-    function prepareData($data)
-    {
-        return mysqli_real_escape_string($this->connect, stripslashes(htmlspecialchars($data)));
-    }
-
-    function logIn($table, $username, $password)
+    public function logIn($table, $username, $password)
     {
         $username = $this->prepareData($username);
         $password = $this->prepareData($password);
-        $this->sql = "select * from " . $table . " where username = '" . $username . "'";
-        $result = mysqli_query($this->connect, $this->sql);
-        $row = mysqli_fetch_assoc($result);
-        if (mysqli_num_rows($result) != 0) {
-            $dbusername = $row['username'];
-            $dbpassword = $row['password'];
-            if ($dbusername == $username && password_verify($password, $dbpassword)) {
-                $login = true;
-            } else $login = false;
-        } else $login = false;
-
-        return $login;
+        $this->sql = "SELECT * FROM " . $table . " WHERE username = '" . $username . "' AND password = '" . $password . "'";
+        $result = $this->conn->query($this->sql);
+        if ($result->num_rows > 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    function signUp($table, $fullname, $email, $username, $password)
+    public function signUp($table, $fullname, $email, $username, $password)
     {
         $fullname = $this->prepareData($fullname);
+        $email = $this->prepareData($email);
         $username = $this->prepareData($username);
         $password = $this->prepareData($password);
-        $email = $this->prepareData($email);
-        $password = password_hash($password, PASSWORD_DEFAULT);
-        $this->sql =
-            "INSERT INTO " . $table . " (fullname, username, password, email) VALUES ('" . $fullname . "','" . $username . "','" . $password . "','" . $email . "')";
-        if (mysqli_query($this->connect, $this->sql)) {
+        $this->sql = "INSERT INTO " . $table . " (fullname, email, username, password) VALUES ('" . $fullname . "', '" . $email . "', '" . $username . "', '" . $password . "')";
+        if ($this->conn->query($this->sql) === TRUE) {
             return true;
-        } else return false;
+        } else {
+            return false;
+        }
     }
-
 }
 
 ?>
